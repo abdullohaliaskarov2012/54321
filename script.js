4bbce7e1ff07d60d97399570f4eeeb1d11b8d285
abdullohaliaskarov2012 <aliaskarovabdulloh@gmail.com>
2025-01-21 06:06:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function qiyriq() {
+    const pricePerItem = 45000; 
+    const quantity = prompt("Сколько хотите?");
+
+    switch (true) {
+        
+        default:
+            const totalPrice = quantity * pricePerItem; // Расчёт общей стоимости
+            alert(`Общая стоимость: ${totalPrice} сум`);
+    }
+}
